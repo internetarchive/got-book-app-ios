a0ee7777cca4d3760c3e2d87c10dfc1fb8148a3b
Internet Archive <archive@ilovelamp.noise>
2022-09-30 18:26:23 -0700

    init blank app

---
+++ README.md
@@ -0,0 +1,11 @@
+## 
+
+### Got Book?
+
+A mobile app to check whether the Internet Archive has a book or not.
+
+
+
+## License
+
+Copyright © 2022 Internet Archive. All rights reserved.
---
+++ app/GotBook/GotBook/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GotBook
+//
+//  Created by Internet Archive on 9/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
